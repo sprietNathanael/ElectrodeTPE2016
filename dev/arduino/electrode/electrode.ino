0238650f2de90bf13d11df998354b14d580b2923
Nathanaël Spriet <nathanael.spriet@gmail.com>
2016-12-28 23:04:46 +0100

    Adds a mere function to light 10 leds
    Uses the principle of the persistence of vision to enlight only one led at once (with a delay of 0.1 millisec of enlightment)

--- dev/arduino/electrode/electrode.ino
+++ dev/arduino/electrode/electrode.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+const int ledNumber = 10;
+char leds[ledNumber];
+const char firstLed = 1;
 
+void setup()
+{
+  for(char ledIncrement = 0;ledIncrement < ledNumber;ledIncrement++)
+  {
+    leds[ledIncrement] = firstLed+ledIncrement;
+    pinMode(leds[ledIncrement], OUTPUT);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  lightWholeLedsArray(leds,ledNumber);
 }
+
+void lightWholeLedsArray(char *ledsArray, int arraySize)
+{
+  for(int i = 0; i < arraySize; i++)
+  {
+    digitalWrite(ledsArray[i], HIGH);
+    delay(0.1);
+    digitalWrite(ledsArray[i], LOW);
+  }
+}
+
